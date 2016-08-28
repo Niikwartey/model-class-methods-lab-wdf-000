7b33cc5f7ebed417ea9b485482f818f66e3929c0
Derick Quartey <niikwartey1@gmail.com>
2016-08-28 18:33:27 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: :classifications).where("classifications.name is 'Catamaran'")
+  end
+
+  def self.sailors
+    joins(boats: :classifications).where("classifications.name is 'Sailboat'").distinct
+  end
+
+  def self.motorboats
+    joins(boats: :classifications).where("classifications.name is 'Motorboat'").distinct
+  end
+
+  def self.talented_seamen
+    # joins(boats: :classifications).where(classifications: {name: ['Motorboat', 'Sailboat']}).distinct
+    where("id IN (?)", sailors.pluck(:id) & motorboats.pluck(:id)).distinct
+  end
+
+  def self.non_sailors
+    where("id NOT IN (?)", sailors.pluck(:id)).distinct
+  end
 end
